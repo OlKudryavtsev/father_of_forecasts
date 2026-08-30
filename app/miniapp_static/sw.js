861ec9fd2430f5c84b3ba50eba6e17add875a2ef
Oleg <35740390+OlKudryavtsev@users.noreply.github.com>
2026-08-30 15:33:04 +0300

    chore: bump static service worker version

--- app/miniapp_static/sw.js
+++ app/miniapp_static/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '3.9.5';
+const APP_VERSION = '3.9.6';
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
